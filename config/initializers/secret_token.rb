a5b10028d9cd8eff5dedf28cebe9688c4d8b7367
cotter <cotter.phinney@gmail.com>
2013-11-28 22:54:43 -0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialbot::Application.config.secret_key_base = 'b9f20a5b600237d7973546cb4bcceafbe64b7fb1ef22ffff752e43c2c793e3f387efc9ab3c5c0eeba0c2ef8affd8b9507a8c46d85b31a85788784a7b1441ff93'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Socialbot::Application.config.session_store :cookie_store, key: '_socialbot_session'
