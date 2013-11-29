1caa3928f955d44d7cd14048be82d2ba6d7f1635
Omar Qazi <omar@predpol.com>
2013-11-29 09:10:05 -0500

    fixed error in messages model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
 	belongs_to :user
   
-  belongs_to :parent, :class_name => "Message", :inverse_of => :response
+  belongs_to :parent, :class_name => "Message"
   has_many :responses, :foreign_key => "parent_id", :class_name => "Message"
 end
