51ddcfd1d88837845b89e1786c18b7d634fe82c0
cotter <cotter.phinney@gmail.com>
2013-12-01 16:57:10 -0500

    display messages on index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 	before_filter :authenticate_admin!, :only => :admin
 
 	def index
-		render text: "index"
+		@messages = Message.all
 	end
 
 	def admin
