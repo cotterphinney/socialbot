9acb53f65f44e55d31f526de2a837400d14473fa
cotter <cotter.phinney@gmail.com>
2013-12-03 00:29:41 -0500

    thread conversation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,17 @@ class PagesController < ApplicationController
 	before_filter :authenticate_admin!, :only => :admin
 
 	def index
-		@messages = Message.all
+		messages = Message.where(:artificial => nil)
+		@conversation = {}
+
+		messages.each do |m|
+			if m.parent_id?
+				response = Message.find(m.parent_id).body
+			else
+				response = false
+			end
+			@conversation[m.body] = response
+		end
 	end
 
 	def admin
