97bab6d52a605c11ab68eb185cf824a076a45d02
cotter <cotter.phinney@gmail.com>
2013-12-02 23:48:52 -0500

    change to all

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 	before_filter :authenticate_admin!, :only => :admin
 
 	def index
-		@messages = Message.limit(200)
+		@messages = Message.all
 	end
 
 	def admin
